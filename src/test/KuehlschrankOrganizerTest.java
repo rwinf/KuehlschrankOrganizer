f8f9c7e0cdc6a24050d48f1b6dbf0199caadf83b
rwinf <rwinfoc@gmail.com>
2024-06-09 17:08:40 +0200

    updated 2

--- src/test/KuehlschrankOrganizerTest.java
+++ src/test/KuehlschrankOrganizerTest.java
@@ -1,9 +1,6 @@
-package helper;
+package test;
 
-import com.sun.jdi.Value;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class KuehlschrankOrganizerTest {
 
